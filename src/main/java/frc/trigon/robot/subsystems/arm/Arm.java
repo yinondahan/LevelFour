5e41bba52a75e3a05675ca5029ccbdac8d95000d
YinonError404 <ynnon.dahan@gmail.com>
2026-01-05 21:00:45 +0200

    goom65/

--- src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
@@ -1,23 +1,22 @@
 package frc.trigon.robot.subsystems.arm;
 
+import com.ctre.phoenix6.controls.DynamicMotionMagicVoltage;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
-import com.ctre.phoenix6.hardware.CANcoder;
-import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.trigon.lib.hardware.phoenix6.cancoder.CANcoderEncoder;
+import frc.trigon.lib.hardware.phoenix6.cancoder.CANcoderSignal;
 import frc.trigon.lib.hardware.phoenix6.talonfx.TalonFXMotor;
 
 
 public class Arm extends SubsystemBase {
     private final TalonFXMotor motor = ArmConstants.MOTOR;
-    private final PIDController pidController = ArmConstants.PID;
-    private final ArmFeedforward feedforward = ArmConstants.FEEDFORWARD;
     private final VoltageOut voltageRequest = new VoltageOut(0).withEnableFOC(ArmConstants.FOC_ENABLED);
-    private final CANcoder encoder = ArmConstants.ENCODER;
+    private final DynamicMotionMagicVoltage positionRequest = new DynamicMotionMagicVoltage(0, ArmConstants.MAX_VELOCITY, ArmConstants.MAX_ACCELERATION, ArmConstants.MAX_JERK).withEnableFOC(ArmConstants.FOC_ENABLED);
+    private final CANcoderEncoder encoder = ArmConstants.ENCODER;
     private final TrapezoidProfile profile = new TrapezoidProfile(ArmConstants.CONSTRAINTS);
     private TrapezoidProfile.State goalState = new TrapezoidProfile.State();
     private TrapezoidProfile.State initialState = new TrapezoidProfile.State();
@@ -36,9 +35,12 @@ public class Arm extends SubsystemBase {
     }
 
     void followSetpoint(Rotation2d setpoint) {
-        final double pidOutput = calculatePIDOutput(setpoint);
-        final double ffOutput = calculateFeedForward();
         setTargetVoltage(pidOutput + ffOutput);
+    }
+
+    void setTargetAngle(Rotation2d targetAngle) {
+        final Rotation2d rotations = Rotation2d.fromRotations(targetAngle.getDegrees());
+        motor.setControl(positionRequest.withPosition());
     }
 
     void stop() {
@@ -48,7 +50,7 @@ public class Arm extends SubsystemBase {
     void initializeMotionProfile(Rotation2d targetPosition) {
         initialState = new TrapezoidProfile.State(
                 getCurrentAngle().getRotations(),
-                encoder.getVelocity().getValueAsDouble()
+                encoder.getSignal(CANcoderSignal.VELOCITY)
         );
 
         goalState = new TrapezoidProfile.State(targetPosition.getRotations(), 0);
@@ -77,7 +79,7 @@ public class Arm extends SubsystemBase {
     }
 
     private Rotation2d getCurrentAngle() {
-        final double rotations = ArmConstants.ANGLE_SIGNAL.refresh().getValueAsDouble();
+        final double rotations = ArmConstants.ENCODER.getSignal(CANcoderSignal.POSITION);
         return Rotation2d.fromRotations(rotations);
     }
 
--- src/main/java/frc/trigon/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/ArmConstants.java
@@ -1,105 +1,74 @@
 package frc.trigon.robot.subsystems.arm;
 
-import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.signals.*;
-
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.util.Color;
-
 import frc.trigon.lib.hardware.RobotHardwareStats;
 import frc.trigon.lib.hardware.phoenix6.talonfx.TalonFXMotor;
 import frc.trigon.lib.hardware.simulation.SingleJointedArmSimulation;
 import frc.trigon.lib.utilities.Conversions;
 import frc.trigon.lib.utilities.mechanisms.SingleJointedArmMechanism2d;
+import frc.trigon.lib.hardware.phoenix6.cancoder.CANcoderEncoder;
+import frc.trigon.lib.hardware.phoenix6.cancoder.CANcoderSignal;
 
 public class ArmConstants {
-
     private static final int
-            MOTOR_ID = 1,
-            ENCODER_ID = 1;
-
+            MOTOR_ID = 1,// motor's id
+            ENCODER_ID = 1;// encoder's id
     private static final String
-            MOTOR_NAME = "ArmMotor",
-            ENCODER_NAME = "ArmEncoder";
-
+            MOTOR_NAME = "ArmMotor",//motor's name
+            ENCODER_NAME = "ArmEncoder";// encoder's name
     static final TalonFXMotor MOTOR = new TalonFXMotor(MOTOR_ID, MOTOR_NAME);
-    static final CANcoder ENCODER = new CANcoder(ENCODER_ID, ENCODER_NAME);
-
-    static final StatusSignal<Angle> ANGLE_SIGNAL = ENCODER.getPosition();
+    static final CANcoderEncoder ENCODER = new CANcoderEncoder(ENCODER_ID, ENCODER_NAME);
+    static final double GEAR_RATIO = 42;//ill take this as an example when the motor spins 42 times the arm rotates once
 
     static final double
-            GEAR_RATIO = 42,
-            ARM_LENGTH_METERS = 0.52,
-            ARM_MASS_KG = 3.5,
-            MOTOR_CURRENT_LIMIT = 50;
-
+            LENGTH_METERS = 0.52,
+            MASS_KILOGRAMS = 3.5;
+    private static final boolean SHOULD_ARM_SIMULATE_GRAVITY = true;
+    private static final DCMotor GEARBOX = DCMotor.getKrakenX60Foc(1);//creates a motor model for simulation that representing a real motor
     static final Rotation2d
-            MIN_ANGLE = Rotation2d.fromDegrees(0),
-            MAX_ANGLE = Rotation2d.fromDegrees(360);
+            MINIMUM_ANGLE = Rotation2d.fromDegrees(0),
+            MAXIMUM_ANGLE = Rotation2d.fromDegrees(360);
+    private static final SingleJointedArmSimulation ARM_SIMULATION = new SingleJointedArmSimulation(
+            GEARBOX,
+            GEAR_RATIO,
+            LENGTH_METERS,
+            MASS_KILOGRAMS,
+            MINIMUM_ANGLE,
+            MAXIMUM_ANGLE,
+            SHOULD_ARM_SIMULATE_GRAVITY
+    );
 
-    private static final double ANGLE_ENCODER_GRAVITY_OFFSET = 0;
-
-    static final double POSITION_OFFSET_FROM_GRAVITY_OFFSET =
-            RobotHardwareStats.isSimulation()
-                    ? -Conversions.degreesToRotations(90)
-                    : -ANGLE_ENCODER_GRAVITY_OFFSET;
-
-    private static final double
-            MAX_VELOCITY = RobotHardwareStats.isSimulation() ? 2.46 : 0,
-            MAX_ACCELERATION = RobotHardwareStats.isSimulation() ? 67.2 : 0;
-
-    static final TrapezoidProfile.Constraints CONSTRAINTS =
-            new TrapezoidProfile.Constraints(MAX_VELOCITY, MAX_ACCELERATION);
-
-    private static final double
-            P = RobotHardwareStats.isSimulation() ? 34 : 0,
-            I = 0,
-            D = RobotHardwareStats.isSimulation() ? 3 : 0;
-
-    private static final double
-            KS = RobotHardwareStats.isSimulation() ? 0.026 : 0,
-            KV = RobotHardwareStats.isSimulation() ? 4.87 : 0,
-            KA = RobotHardwareStats.isSimulation() ? 0.178 : 0,
-            KG = RobotHardwareStats.isSimulation() ? 0.112 : 0;
-
-    static final PIDController PID = new PIDController(P, I, D);
-    static final ArmFeedforward FEEDFORWARD = new ArmFeedforward(KS, KG, KV, KA);
-
-    static final Rotation2d TOLERANCE = Rotation2d.fromDegrees(2);
-
-    static final boolean FOC_ENABLED = true;
-
-    private static final DCMotor GEARBOX = DCMotor.getKrakenX60Foc(1);
-
-    static final SingleJointedArmSimulation SIMULATION =
-            new SingleJointedArmSimulation(
-                    GEARBOX,
-                    GEAR_RATIO,
-                    ARM_LENGTH_METERS,
-                    ARM_MASS_KG,
-                    MIN_ANGLE,
-                    MAX_ANGLE,
-                    true
-            );
-
+    private static final String MECHANISM_NAME = "Arm";
     static final SingleJointedArmMechanism2d MECHANISM =
             new SingleJointedArmMechanism2d(
-                    "Arm",
-                    ARM_LENGTH_METERS,
+                    MECHANISM_NAME,
+                    LENGTH_METERS,
                     Color.kBlue
-            );
+            );//defining the mechanism of the arm in the simulation
+
+    static final double
+            MAX_VELOCITY = RobotHardwareStats.isSimulation() ? 2.46 : 0, //same here
+            MAX_ACCELERATION = RobotHardwareStats.isSimulation() ? 67.2 : 0, //same here
+            MAX_JERK = MAX_ACCELERATION * 10;
+    static final TrapezoidProfile.Constraints CONSTRAINTS =
+            new TrapezoidProfile.Constraints(MAX_VELOCITY, MAX_ACCELERATION); //define the constraints
+    static final Rotation2d TOLERANCE = Rotation2d.fromDegrees(2);
+    private static final double ANGLE_ENCODER_GRAVITY_OFFSET = 0;
+    static final double POSITION_OFFSET_FROM_GRAVITY_OFFSET =
+            RobotHardwareStats.isSimulation()
+                    ? -Conversions.degreesToRotations(90) // "?" means "if it's true" then that the value of the variable
+                    : -ANGLE_ENCODER_GRAVITY_OFFSET; // ":" means "if it's false"
+    static final boolean FOC_ENABLED = true;//who doesn't know wot de FOC is this?
 
     static {
-        configureMotor();
         configureEncoder();
+        configureMotor();
     }
 
     private static void configureMotor() {
@@ -108,29 +77,30 @@ public class ArmConstants {
         config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
         config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
 
-        config.Feedback.RotorToSensorRatio = GEAR_RATIO;
-        config.Feedback.FeedbackRemoteSensorID = ENCODER.getDeviceID();
-        config.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+        config.Feedback.RotorToSensorRatio = GEAR_RATIO; //configuring the gear ratio
+        config.Feedback.FeedbackRemoteSensorID = ENCODER.getID();// Tells the motor to read from this encoder
+        config.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;// the source of the encoder
 
-        config.Slot0.kP = P;
-        config.Slot0.kI = I;
-        config.Slot0.kD = D;
-        config.Slot0.kS = KS;
-        config.Slot0.kV = KV;
-        config.Slot0.kA = KA;
-        config.Slot0.kG = KG;
+        config.Slot0.kP = RobotHardwareStats.isSimulation() ? 34 : 0; // slot is a different settings for the same motor that you can change in the middle of a run
+        config.Slot0.kI = 0;
+        config.Slot0.kD = RobotHardwareStats.isSimulation() ? 3 : 0;
+        config.Slot0.kS = RobotHardwareStats.isSimulation() ? 0.026 : 0;
+        config.Slot0.kV = RobotHardwareStats.isSimulation() ? 4.87 : 0;
+        config.Slot0.kA = RobotHardwareStats.isSimulation() ? 0.178 : 0;
+        config.Slot0.kG = RobotHardwareStats.isSimulation() ? 0.112 : 0;
 
-        config.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
-        config.Slot0.StaticFeedforwardSign = StaticFeedforwardSignValue.UseVelocitySign;
+        config.Slot0.GravityType = GravityTypeValue.Arm_Cosine; // Automatically adds power to hold the arm up against gravity
+        config.Slot0.StaticFeedforwardSign = StaticFeedforwardSignValue.UseVelocitySign; // adds a little extra power in the right direction so the arm moves smoothly
 
-        config.MotionMagic.MotionMagicCruiseVelocity = MAX_VELOCITY;
-        config.MotionMagic.MotionMagicAcceleration = MAX_ACCELERATION;
+        config.MotionMagic.MotionMagicCruiseVelocity = MAX_VELOCITY;//setting the max velocity for the motion magic // motion magic: moves the arm smoothly to a position without shaking
+        config.MotionMagic.MotionMagicAcceleration = MAX_ACCELERATION;//same here but with acceleration
+        config.MotionMagic.MotionMagicJerk = MAX_JERK; // makes the arm move smoothly
 
-        config.CurrentLimits.StatorCurrentLimitEnable = true;
-        config.CurrentLimits.StatorCurrentLimit = MOTOR_CURRENT_LIMIT;
+        config.CurrentLimits.StatorCurrentLimitEnable = true; //turns on the current limit to protect the motor
+        config.CurrentLimits.StatorCurrentLimit = 50;//sets the current limit
 
-        MOTOR.applyConfiguration(config);
-        MOTOR.setPhysicsSimulation(SIMULATION);
+        MOTOR.applyConfiguration(config);//applying all the motor configuration
+        MOTOR.setPhysicsSimulation(ARM_SIMULATION); // Makes the motor act like the arm is moving in simulation
     }
 
     private static void configureEncoder() {
@@ -140,8 +110,11 @@ public class ArmConstants {
         config.MagnetSensor.MagnetOffset = ANGLE_ENCODER_GRAVITY_OFFSET;
         config.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 1;
 
-        ENCODER.getConfigurator().apply(config);
-        ANGLE_SIGNAL.setUpdateFrequency(100);
+        ENCODER.applyConfiguration(config);
+        ENCODER.setSimulationInputsFromTalonFX(MOTOR);//makes the encoder follow the motor in the simulation
+
+        ENCODER.registerSignal(CANcoderSignal.POSITION, 100);
+        ENCODER.registerSignal(CANcoderSignal.VELOCITY, 100);//updates the encoder position and speed every 100ms
     }
 
     public enum ArmState {
